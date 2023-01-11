e4345138258eaf3c8b812ac65951ee2bdc8de9ca
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-01-11 21:06:28 +0530

    [Aniket]-Fib Number Series code

--- Fibseries/Program.cs
+++ Fibseries/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Logical Problems!");
+            Console.WriteLine("***FibseriesCode***");
+            int a = 0, b = 1, c;
+            Console.WriteLine("Enter the number:");
+            int Num = Convert.ToInt32(Console.ReadLine());
+            Console.Write(a + " " + b + " ");
+
+            for ( int i = 2; i < Num; i++)
+            {
+                c = a + b;
+                Console.Write(c + " ");
+                a = b;
+                b = c;
+            }
         }
     }
 }
